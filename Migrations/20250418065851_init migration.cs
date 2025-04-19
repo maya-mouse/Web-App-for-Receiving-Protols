d38da69e1260cc8c31ca520c442fed917900d2a3
maya-mouse <120753794+maya-mouse@users.noreply.github.com>
2025-04-19 13:29:47 +0300

    Add files via upload

---
+++ Migrations/20250418065851_init migration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkingReferenceBasics.Migrations
+{
+    /// <inheritdoc />
+    public partial class initmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "adapters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DnsSuffix = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDnsEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    IsDynamicDnsEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    InterfaceType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhysicalAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SuppotingIp4 = table.Column<bool>(type: "bit", nullable: false),
+                    SuppotingIp6 = table.Column<bool>(type: "bit", nullable: false),
+                    isRecieveOnly = table.Column<bool>(type: "bit", nullable: false),
+                    Multicast = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_adapters", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "adapters");
+        }
+    }
+}
---
+++ Migrations/20250418165923_Adding protocols info.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkingReferenceBasics.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addingprotocolsinfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ethernetProtocols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourceHardwareAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DestinationHardwareAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ethernetProtocols", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ipProtocols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourceAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DestinationAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeaderLenght = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeToLive = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ipProtocols", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tcpProtocols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourcePort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DestinationPort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Flags = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tcpProtocols", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "udpProtocols",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourcePort = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DestinationPort = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_udpProtocols", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ethernetProtocols");
+
+            migrationBuilder.DropTable(
+                name: "ipProtocols");
+
+            migrationBuilder.DropTable(
+                name: "tcpProtocols");
+
+            migrationBuilder.DropTable(
+                name: "udpProtocols");
+        }
+    }
+}
