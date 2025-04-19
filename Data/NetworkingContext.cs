666f01a25e6e332ddb917284b92ab3b7b76eafcc
maya-mouse <120753794+maya-mouse@users.noreply.github.com>
2025-04-19 13:28:35 +0300

    Add files via upload

---
+++ Data/NetworkingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetworkingReferenceBasics.Models;
+namespace NetworkingReferenceBasics.Data
+{
+    public class NetworkingContext : DbContext
+    {
+        public NetworkingContext(DbContextOptions<NetworkingContext> options) : base(options) { }
+
+        public DbSet<AdapterModel> adapters { get; set; }
+        public DbSet<EthernetModel> ethernetProtocols { get; set; }
+        public DbSet<IpModel> ipProtocols { get; set; }
+        public DbSet<TcpModel>  tcpProtocols { get; set; }
+        public DbSet<UdpModel> udpProtocols { get; set; }
+    }
+}
